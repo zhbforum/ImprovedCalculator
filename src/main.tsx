fa8d9394fd9a8eafb62441249edde9068d1d2dd0
zhbforum <lyosha.minakoff@gmail.com>
2025-12-18 23:21:31 +0100

    chore(i18n): initialize i18n in app entry

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "@/theme/ThemeProvider";
+import "./i18n";
 
 createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
